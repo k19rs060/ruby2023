9d5e97e046689d928631d5659c16ed6eae6fe557
Rena Zaitsu <k23gjk04@st.kyusan-u.ac.jp>
2023-05-08 16:42:10 +0900

    条件分岐

--- first.rb
+++ first.rb
@@ -1,5 +1,5 @@
 puts "Hello" # puts = put string
-puts "ksu"
+puts ("ksu") # ()があっても良い
 
 i=5
 s="ksu"
@@ -12,3 +12,19 @@ puts i-j
 puts i*j
 puts i/j
 puts i%j
+
+#条件分岐
+if i%2 == 0
+    puts "even"
+else
+    puts "odd" # 複数かける
+    puts "kisu"
+end
+
+if i%3 == 0
+    puts "0"
+elsif i%3==1 # else if のこと
+    puts "1"
+else
+    puts "2"
+end
