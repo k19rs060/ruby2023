28c9f95ff026c3b39ac451b72169f3a0fec8b45b
Rena Zaitsu <k23gjk04@st.kyusan-u.ac.jp>
2023-05-15 16:42:54 +0900

    練習問題

---
+++ name.rb
@@ -0,0 +1,14 @@
+# name?と表示
+# 変数にキーボードから名前を入力
+# こんにちは、〇〇 という形式で名前を表示
+puts "name?"
+n=gets
+puts "こんにちは、"+n
+
+# number? と表示
+# 変数にキーボードから数値を入力
+# 入力された値+1を画面に表示
+print "number?"
+m=gets
+# m=m.to_i
+puts m.to_i+1
