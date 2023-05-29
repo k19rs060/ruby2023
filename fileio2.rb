dc5909145dcbccc85119d2ec1bde848e307ac313
Rena Zaitsu <k23gjk04@st.kyusan-u.ac.jp>
2023-05-29 17:00:05 +0900

    カッコについての補足を追加

--- fileio2.rb
+++ fileio2.rb
@@ -4,6 +4,6 @@ name = gets
 
 # name.txt というファイルに変数 name の値を出力
 # モード選択 r:読み込み w:書き込み a:追記
-File.open("name.txt", "w") do |f|
+File.open("name.txt", "w") do |f| # File.open "name.txt", "w" do |f| カッコがなくても動く
     f.puts name
 end
