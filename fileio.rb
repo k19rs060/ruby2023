7449bbcaf5b4fe31ddce385a3b3b552431f5b959
Rena Zaitsu <k23gjk04@st.kyusan-u.ac.jp>
2023-05-29 16:36:05 +0900

    open,close を File open do に変更

--- fileio.rb
+++ fileio.rb
@@ -1,10 +1,8 @@
 # ファイルをopenする
-f=File.open "ls.out", "r"
-
-i=1
-while line=f.gets
-    puts i.to_s+" : "+line
-    i+=1
-end
-
-f.close # オープンしたらクローズする(メモリリークを起こすため)
+File.open("ls.out", "r") do |f| # f=File.open "ls.out", "r"
+    i=1
+    while line=f.gets
+        puts i.to_s+" : "+line
+        i+=1
+    end
+end # f.close # オープンしたらクローズする(メモリリークを起こすため)
