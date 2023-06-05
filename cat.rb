7505f245a0bd91ba2f9c05c1644e3fe537f14040
Rena Zaitsu <k23gjk04@st.kyusan-u.ac.jp>
2023-06-05 17:16:42 +0900

    -n(オプション) 行番号を実装

--- cat.rb
+++ cat.rb
@@ -1,5 +1,35 @@
-File.open(ARGV[0], "r") do |f|
+#! /usr/bin/env ruby
+#rubyではif内スコープがない
+
+if ARGV[0]=="-n"
+    nopt=true
+    file=ARGV[1]
+else
+    nopt=false
+    file=ARGV[0]
+end
+
+linnum=1
+File.open(file, "r") do |f|
     while line=f.gets
+        printf("%5d",linnum) if nopt
+        linnum += 1
         puts line
     end
 end
+
+#if ARGV[0]=="-n"
+#    File.open(ARGV[1], "r") do |f|
+#        i=1
+#        while line=f.gets
+#            puts i.to_s+" : "+line
+#            i+=1
+#        end
+#    end
+#else
+#    File.open(ARGV[0], "r") do |f|
+#        while line=f.gets
+#            puts line
+#        end
+#    end
+#end
