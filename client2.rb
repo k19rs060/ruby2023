f3379a496edaf06c0aeba11619c4e47e3e40c8fa
Rena Zaitsu <k23gjk04@st.kyusan-u.ac.jp>
2023-06-12 17:02:25 +0900

    httpのbody部分だけ表示(表示方法2種)

--- client2.rb
+++ client2.rb
@@ -5,14 +5,10 @@ s=TCPSocket.open("www.is.kyusan-u.ac.jp", port)
 s.puts("GET /~toshi/ HTTP/1.0") #sに出力する
 s.puts #sに出力する
 
+is_body=false
 while line=s.gets
     line.chomp!
-    break if line==""
+    puts line if is_body
+    is_body=true if line==""
 end
-
-while line=s.gets
-    line.chomp!
-    puts line
-end
-
 s.close
