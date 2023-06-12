a7ba7322a5216f84cf26a4b70041d5c23eee5db9
Rena Zaitsu <k23gjk04@st.kyusan-u.ac.jp>
2023-06-12 16:44:16 +0900

    httpのヘッド情報のみ表示

--- client1.rb
+++ client1.rb
@@ -2,8 +2,14 @@ require "socket" #javaでいうところのimport
 
 port=80
 s=TCPSocket.open("www.is.kyusan-u.ac.jp", port)
-s.puts("GET /~toshi/index.html")
+s.puts("GET /~toshi/ HTTP/1.0") #sに出力する
+s.puts #sに出力する
 while line=s.gets
+    line.chomp! #line=line.chomp
+    break if line==""
+    #if line==""
+    #    break
+    #end
     puts line
 end
 s.close
