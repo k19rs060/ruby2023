44f0b2d33d3957df5bceaf4b9ecc91c0486cf085
Rena Zaitsu <k23gjk04@st.kyusan-u.ac.jp>
2023-06-12 17:18:02 +0900

    http/1.0 コマンド引数を使用

--- client1.rb
+++ client1.rb
@@ -1,8 +1,11 @@
 require "socket" #javaでいうところのimport
 
 port=80
-s=TCPSocket.open("www.is.kyusan-u.ac.jp", port)
-s.puts("GET /~toshi/ HTTP/1.0") #sに出力する
+host = ARGV[0]
+path = ARGV[1]
+
+s=TCPSocket.open(host, port)
+s.puts("GET #{path} HTTP/1.0") #="GET" + path + "HTTP/1.0"
 s.puts #sに出力する
 
 while line=s.gets
