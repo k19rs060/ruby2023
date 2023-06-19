96bdc9ae719de4a78de8cdc9a425b23f53df7172
Rena Zaitsu <k23gjk04@st.kyusan-u.ac.jp>
2023-06-19 17:02:01 +0900

    -h/-b オプション対応追加

--- client1.rb
+++ client1.rb
@@ -1,5 +1,17 @@
 require "socket" #javaでいうところのimport
 
+disp_head = false
+disp_body = true
+
+if ARGV[0] == "-h"
+    disp_head = true
+    disp_body = false
+    ARGV.shift
+elsif ARGV[0] == "-b"
+    disp_body = true
+    ARGV.shift
+end
+
 port=80
 host = ARGV[0]
 path = ARGV[1]
@@ -9,14 +21,15 @@ s.puts "GET #{path} HTTP/1.1" # ="GET" + path + "HTTP/1.0" ("GET #{path} HTTP/1.
 s.puts "Host: #{host}" # "Host:" + host
 s.puts #sに出力する
 
-while line=s.gets
+while line=s.gets #headのみ
     line.chomp!
     break if line==""
+    puts line if disp_head
 end
 
-while line=s.gets
-    line.chomp!
-    puts line
+while line=s.gets #bodyのみ
+    line.chomp! 
+    puts line if disp_body
 end
 
 s.close
