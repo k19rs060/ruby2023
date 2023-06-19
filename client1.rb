5beaa19b950b765ee473eb365e6367e3324bbb83
Rena Zaitsu <k23gjk04@st.kyusan-u.ac.jp>
2023-06-19 17:01:00 +0900

    http1.1対応

--- client1.rb
+++ client1.rb
@@ -4,8 +4,9 @@ port=80
 host = ARGV[0]
 path = ARGV[1]
 
-s=TCPSocket.open(host, port)
-s.puts("GET #{path} HTTP/1.0") #="GET" + path + "HTTP/1.0"
+s=TCPSocket.open host, port # (host,port)
+s.puts "GET #{path} HTTP/1.1" # ="GET" + path + "HTTP/1.0" ("GET #{path} HTTP/1.1")
+s.puts "Host: #{host}" # "Host:" + host
 s.puts #sに出力する
 
 while line=s.gets
